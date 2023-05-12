71cae7322146616bfa4c0826d13f5f6fdd7c1c8c
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-05-12 18:41:28 -0400

    Finished update to 1.19.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,3 @@
 plugins {
-  id("roundalib") version "0.2.75"
+  id("roundalib") version "0.3.3"
 }
--- src/main/java/me/roundaround/inventorymanagement/compat/roundalib/ConfigControlRegister.java
+++ src/main/java/me/roundaround/inventorymanagement/compat/roundalib/ConfigControlRegister.java
@@ -7,23 +7,26 @@ import me.roundaround.roundalib.client.gui.widget.config.ControlRegistry;
 import me.roundaround.roundalib.client.gui.widget.config.SubScreenControl;
 import me.roundaround.roundalib.config.option.PositionConfigOption;
 import me.roundaround.roundalib.config.value.Position;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public class ConfigControlRegister {
+  private static final MinecraftClient client = MinecraftClient.getInstance();
+
   private ConfigControlRegister() {
   }
 
   public static void init() {
     try {
       ControlRegistry.register(InventoryManagementMod.CONFIG.DEFAULT_POSITION.getId(),
-          ConfigControlRegister::guiOffsetEditScreenControlFactory);
+          ConfigControlRegister::defaultPositionEditScreenControlFactory);
     } catch (ControlRegistry.RegistrationException e) {
       // Deal with this later xD
     }
   }
 
-  private static SubScreenControl<Position, PositionConfigOption> guiOffsetEditScreenControlFactory(
+  private static SubScreenControl<Position, PositionConfigOption> defaultPositionEditScreenControlFactory(
       ConfigListWidget.OptionEntry<Position, PositionConfigOption> parent) {
     SubScreenControl<Position, PositionConfigOption> control =
         new SubScreenControl<>(parent, DefaultPositionEditScreen.getSubScreenFactory());
@@ -32,7 +35,7 @@ public class ConfigControlRegister {
         .getValue()
         .toString()));
 
-    parent.getOption().subscribeToValueChanges((prev, curr) -> {
+    parent.getOption().subscribeToValueChanges(client.currentScreen, (prev, curr) -> {
       ((ButtonWidget) control.children().get(0)).setMessage(Text.literal(curr.toString()));
     });
 
