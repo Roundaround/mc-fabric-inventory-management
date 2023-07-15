bc5f70ecfabb87c94500d46e89659b4ad9abeae2
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-07-14 21:02:14 -0400

    Serialize and deserialize for PerScreenConfig

--- src/main/java/me/roundaround/inventorymanagement/config/value/PerScreenConfig.java
+++ src/main/java/me/roundaround/inventorymanagement/config/value/PerScreenConfig.java
@@ -2,7 +2,6 @@ package me.roundaround.inventorymanagement.config.value;
 
 import me.roundaround.roundalib.config.value.Position;
 
-import java.io.Serializable;
 import java.util.HashMap;
 
 public class PerScreenConfig extends HashMap<String, PerScreenConfig.ScreenConfig> {
@@ -11,6 +10,26 @@ public class PerScreenConfig extends HashMap<String, PerScreenConfig.ScreenConfi
                       ButtonVisibility containerSideSortVisibility,
                       ButtonVisibility containerSideTransferVisibility,
                       Position playerSideOffset,
-                      Position containerSideOffset) implements Serializable {
+                      Position containerSideOffset) {
+    public static HashMap<String, String> serialize(ScreenConfig config) {
+      HashMap<String, String> serialized = new HashMap<>();
+      serialized.put("playerSideSortVisibility", config.playerSideSortVisibility().getId());
+      serialized.put("playerSideTransferVisibility", config.playerSideTransferVisibility().getId());
+      serialized.put("containerSideSortVisibility", config.containerSideSortVisibility().getId());
+      serialized.put("containerSideTransferVisibility",
+          config.containerSideTransferVisibility().getId());
+      serialized.put("playerSideOffset", Position.serialize(config.playerSideOffset()));
+      serialized.put("containerSideOffset", Position.serialize(config.containerSideOffset()));
+      return serialized;
+    }
+
+    public static ScreenConfig deserialize(HashMap<String, String> serialized) {
+      return new ScreenConfig(ButtonVisibility.fromId(serialized.get("playerSideSortVisibility")),
+          ButtonVisibility.fromId(serialized.get("playerSideTransferVisibility")),
+          ButtonVisibility.fromId(serialized.get("containerSideSortVisibility")),
+          ButtonVisibility.fromId(serialized.get("containerSideTransferVisibility")),
+          Position.deserialize(serialized.get("playerSideOffset")),
+          Position.deserialize(serialized.get("containerSideOffset")));
+    }
   }
 }
