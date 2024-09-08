9861b202d7b947df4e1b40de50367af4e7e0f534
Roundaround <roundaroundna@gmail.com>
2024-09-07 23:27:28 -0400

    Adding EnumValue interface

---
+++ src/main/java/me/roundaround/inventorymanagement/config/value/PosRefType.java
@@ -0,0 +1,5 @@
+package me.roundaround.inventorymanagement.config.value;
+
+public enum PosRefType {
+  AUTO, MANUAL_SLOT, BACKGROUND, SCREEN
+}
--- src/main/java/me/roundaround/inventorymanagement/config/value/ScreenPosRefType.java
+++ src/main/java/me/roundaround/inventorymanagement/config/value/ScreenPosRefType.java
@@ -1,10 +1,20 @@
 package me.roundaround.inventorymanagement.config.value;
 
+import me.roundaround.inventorymanagement.InventoryManagementMod;
 import me.roundaround.inventorymanagement.api.positioning.PositionReference;
 import me.roundaround.inventorymanagement.api.positioning.ScreenPositionReference;
+import me.roundaround.roundalib.config.value.EnumValue;
 
-public enum ScreenPosRefType {
-  LEFT, RIGHT, TOP, BOTTOM, CENTER_X, CENTER_Y;
+import java.util.Arrays;
+
+public enum ScreenPosRefType implements EnumValue<ScreenPosRefType> {
+  LEFT("left"), RIGHT("right"), TOP("top"), BOTTOM("bottom"), CENTER_X("centerX"), CENTER_Y("centerY");
+
+  private final String id;
+
+  ScreenPosRefType(String id) {
+    this.id = id;
+  }
 
   public PositionReference<?, ?> get() {
     return switch (this) {
@@ -15,5 +25,34 @@ public enum ScreenPosRefType {
       case CENTER_X -> ScreenPositionReference.centerX();
       case CENTER_Y -> ScreenPositionReference.centerY();
     };
+  }
+
+  @Override
+  public String getId() {
+    return this.id;
+  }
+
+  @Override
+  public String getI18nKey(String modId) {
+    return String.format("%s.posref.screen.%s", InventoryManagementMod.MOD_ID, this.id);
+  }
+
+  @Override
+  public ScreenPosRefType getFromId(String id) {
+    return fromId(id);
+  }
+
+  @Override
+  public ScreenPosRefType getNext() {
+    return values()[(this.ordinal() + 1) % values().length];
+  }
+
+  @Override
+  public ScreenPosRefType getPrev() {
+    return values()[(this.ordinal() + values().length - 1) % values().length];
+  }
+
+  public static ScreenPosRefType fromId(String id) {
+    return Arrays.stream(values()).filter((type) -> type.id.equals(id)).findFirst().orElse(RIGHT);
   }
 }
