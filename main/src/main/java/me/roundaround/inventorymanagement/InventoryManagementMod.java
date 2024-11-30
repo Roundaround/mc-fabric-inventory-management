1717ade34882493345e338fa8fade42ec6e5e164
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-11-30 12:06:57 -0500

    Remove unused logger

--- main/src/main/java/me/roundaround/inventorymanagement/InventoryManagementMod.java
+++ main/src/main/java/me/roundaround/inventorymanagement/InventoryManagementMod.java
@@ -7,12 +7,9 @@ import me.roundaround.inventorymanagement.network.Networking;
 import me.roundaround.inventorymanagement.server.network.ServerNetworking;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.screen.HorseScreenHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public final class InventoryManagementMod implements ModInitializer {
   public static final String MOD_ID = "inventorymanagement";
-  public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
   @Override
   public void onInitialize() {
