7bf11ca41bafc8066ebb51daee61df8492f2a5ba
Roundaround <roundaroundna@gmail.com>
2024-12-13 13:59:15 -0500

    Update RoundaLib gradle for compat mods

--- compat/reinfcore/build.gradle.kts
+++ compat/reinfcore/build.gradle.kts
@@ -1,7 +1,8 @@
 import java.net.URI
 
 plugins {
-  id("roundalib") version "0.8.0-SNAPSHOT"
+  id("fabric-loom") version "1.9-SNAPSHOT"
+  id("roundalib") version "0.9.0-SNAPSHOT"
 }
 
 repositories {
