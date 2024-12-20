a974ee9c8fe2f31be99cc4702f75f0111f26f792
Roundaround <roundaroundna@gmail.com>
2024-12-20 16:12:11 -0500

    Set up WrapperComparator to extend CachingComparator

--- main/src/main/java/me/roundaround/inventorymanagement/inventory/sorting/WrapperComparator.java
+++ main/src/main/java/me/roundaround/inventorymanagement/inventory/sorting/WrapperComparator.java
@@ -2,11 +2,18 @@ package me.roundaround.inventorymanagement.inventory.sorting;
 
 import java.util.Comparator;
 
-public interface WrapperComparator<T> extends Comparator<T> {
+public interface WrapperComparator<T> extends Comparator<T>, CachingComparator {
   Comparator<T> getDelegate();
 
   @Override
   default int compare(T o1, T o2) {
     return this.getDelegate().compare(o1, o2);
+  }
+
+  @Override
+  default void clearCache() {
+    if (this.getDelegate() instanceof CachingComparator caching) {
+      caching.clearCache();
+    }
   }
 }
