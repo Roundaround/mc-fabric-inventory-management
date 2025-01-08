c9ea4385b7161a48ddbcdfc7572d3a24165afa0d
Roundaround <roundaroundna@gmail.com>
2025-01-08 10:40:59 -0500

    Simplified ContainerFirstComparator to use the CONTAINER data component

--- main/src/main/java/me/roundaround/inventorymanagement/inventory/sorting/itemstack/ContainerFirstComparator.java
+++ main/src/main/java/me/roundaround/inventorymanagement/inventory/sorting/itemstack/ContainerFirstComparator.java
@@ -1,30 +1,16 @@
 package me.roundaround.inventorymanagement.inventory.sorting.itemstack;
 
-import me.roundaround.inventorymanagement.inventory.sorting.WrapperComparatorImpl;
-import net.minecraft.block.ShulkerBoxBlock;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.BundleItem;
+import me.roundaround.inventorymanagement.inventory.sorting.CachingComparatorImpl;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
 
 import java.util.Comparator;
-import java.util.List;
-import java.util.function.Predicate;
 
-public class ContainerFirstComparator extends WrapperComparatorImpl<ItemStack> {
+public class ContainerFirstComparator extends CachingComparatorImpl<ItemStack, Boolean> {
   private static ContainerFirstComparator instance;
 
   private ContainerFirstComparator() {
-    super(Comparator.comparing((stack) -> {
-      int index = 0;
-      for (Predicate<ItemStack> matcher : getMatchers()) {
-        if (matcher.test(stack)) {
-          return index;
-        }
-        index++;
-      }
-
-      return null;
-    }, Comparator.nullsLast(Integer::compareTo)));
+    super(Comparator.reverseOrder());
   }
 
   public static ContainerFirstComparator getInstance() {
@@ -34,13 +20,8 @@ public class ContainerFirstComparator extends WrapperComparatorImpl<ItemStack> {
     return instance;
   }
 
-  private static List<Predicate<ItemStack>> getMatchers() {
-    // TODO: Registry/hook for mods to hook in their custom containers
-    //@formatter:off
-    return List.of(
-        (stack) -> stack.getItem() instanceof BlockItem block && block.getBlock() instanceof ShulkerBoxBlock,
-        (stack) -> stack.getItem() instanceof BundleItem
-    );
-    //@formatter:on
+  @Override
+  protected Boolean mapValue(ItemStack stack) {
+    return stack.get(DataComponentTypes.CONTAINER) != null;
   }
 }
