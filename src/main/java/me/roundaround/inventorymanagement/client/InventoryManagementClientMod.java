4765c84c13f1ce9c982adaa6053481bc348b2f4a
Roundaround <roundaroundna@gmail.com>
2025-02-04 13:59:18 -0500

    Remove language experimentation code

--- src/main/java/me/roundaround/inventorymanagement/client/InventoryManagementClientMod.java
+++ src/main/java/me/roundaround/inventorymanagement/client/InventoryManagementClientMod.java
@@ -1,16 +1,12 @@
 package me.roundaround.inventorymanagement.client;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import me.roundaround.inventorymanagement.InventoryManagementMod;
 import me.roundaround.inventorymanagement.api.ButtonRegistry;
 import me.roundaround.inventorymanagement.api.InventoryManagementEntrypointHandler;
 import me.roundaround.inventorymanagement.api.positioning.Coords;
 import me.roundaround.inventorymanagement.api.positioning.PositioningFunction;
 import me.roundaround.inventorymanagement.api.positioning.SlotPositionReference;
-import me.roundaround.inventorymanagement.client.network.ClientNetworking;
 import me.roundaround.inventorymanagement.client.option.KeyBindings;
-import me.roundaround.inventorymanagement.event.ResourcesReloadedEvent;
 import me.roundaround.inventorymanagement.inventory.InventoryHelper;
 import me.roundaround.inventorymanagement.mixin.HorseScreenHandlerAccessor;
 import me.roundaround.inventorymanagement.roundalib.client.gui.GuiUtil;
@@ -19,21 +15,10 @@ import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
-import net.minecraft.client.resource.language.LanguageManager;
-import net.minecraft.client.resource.language.TranslationStorage;
-import net.minecraft.registry.Registries;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.screen.*;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class InventoryManagementClientMod implements ClientModInitializer {
@@ -52,95 +37,6 @@ public class InventoryManagementClientMod implements ClientModInitializer {
         ));
 
     this.initButtonRegistry();
-
-    ResourcesReloadedEvent.EVENT.register((client) -> {
-      ClientNetworking.trySendRecalculatePacket();
-
-      // TODO: Figure out if all languages have a clean way to "trim" colors from the strings...
-      LinkedHashMap<String, LinkedHashMap<String, String>> translations = new LinkedHashMap<>();
-
-      LanguageManager languageManager = client.getLanguageManager();
-      ResourceManager resourceManager = client.getResourceManager();
-      languageManager.getAllLanguages().forEach((languageCode, languageDef) -> {
-        ArrayList<String> languageCodes = new ArrayList<>(2);
-        languageCodes.add("en_us");
-        boolean rightToLeft = false;
-        if (!languageCode.equals("en_us")) {
-          languageCodes.add(languageCode);
-          rightToLeft = languageDef.rightToLeft();
-        }
-
-        TranslationStorage translationStorage = TranslationStorage.load(resourceManager, languageCodes, rightToLeft);
-        Registries.ITEM.forEach((item) -> {
-          String translationKey = item.getTranslationKey();
-          if (translationKey.contains("_wool")) {
-            translations.computeIfAbsent(languageCode, (key) -> new LinkedHashMap<>())
-                .put(translationKey, translationStorage.get(translationKey).toLowerCase());
-          }
-        });
-      });
-
-      Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-      Path msSuppLangsFile = FabricLoader.getInstance().getGameDir().resolve("microsoft_supported_langs.json");
-      MicrosoftSupportedLanguages msSuppLangs;
-      try {
-        msSuppLangs = gson.fromJson(Files.newBufferedReader(msSuppLangsFile), MicrosoftSupportedLanguages.class);
-
-        for (var langEntry : translations.entrySet()) {
-          HashMap<String, String> langTranslations = langEntry.getValue();
-          List<String> strings = List.copyOf(langTranslations.values());
-          String longestCommon = findLongestCommonSubstring(strings);
-          langTranslations.put("Longest Common Substring", longestCommon.trim());
-          boolean supportedByMs = msSuppLangs.translation.containsKey(getLangWithoutCountry(langEntry.getKey()));
-          langTranslations.put("Supported by MS", String.valueOf(supportedByMs));
-        }
-
-        Path outFile = FabricLoader.getInstance().getGameDir().resolve("language_dump_wool.json");
-        try (var writer = Files.newBufferedWriter(outFile)) {
-          writer.write(gson.toJson(translations));
-        }
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    });
-  }
-
-  private static String getLangWithoutCountry(String withCountry) {
-    int index = withCountry.indexOf("_");
-    if (index == -1) {
-      return withCountry;
-    }
-    return withCountry.substring(0, index);
-  }
-
-  private static String findLongestCommonSubstring(List<String> strings) {
-    if (strings.isEmpty()) {
-      return "";
-    }
-
-    String first = strings.getFirst();
-    int len = first.length();
-
-    for (int substringLength = len; substringLength > 0; substringLength--) {
-      for (int start = 0; start <= len - substringLength; start++) {
-        String substring = first.substring(start, start + substringLength);
-
-        boolean allContain = true;
-        for (String str : strings) {
-          if (!str.contains(substring)) {
-            allContain = false;
-            break;
-          }
-        }
-
-        if (allContain) {
-          return substring;
-        }
-      }
-    }
-
-    return "";
   }
 
   private void initButtonRegistry() {
@@ -211,10 +107,5 @@ public class InventoryManagementClientMod implements ClientModInitializer {
     FabricLoader.getInstance()
         .getEntrypointContainers("inventorymanagement", InventoryManagementEntrypointHandler.class)
         .forEach((entrypoint) -> entrypoint.getEntrypoint().onInventoryManagementInit());
-  }
-
-  private record MicrosoftSupportedLanguages(HashMap<String, Object> translation,
-                                             HashMap<String, Object> transliteration,
-                                             HashMap<String, Object> dictionary) {
   }
 }
