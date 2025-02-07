e1add29fa52e8bb0b98ef7125774779e4a0b002d
Roundaround <roundaroundna@gmail.com>
2025-02-07 10:37:11 -0500

    Improve decorated pot comparator, add tests

--- src/main/java/me/roundaround/inventorymanagement/inventory/sorting/itemstack/DecoratedPotComparator.java
+++ src/main/java/me/roundaround/inventorymanagement/inventory/sorting/itemstack/DecoratedPotComparator.java
@@ -2,34 +2,74 @@ package me.roundaround.inventorymanagement.inventory.sorting.itemstack;
 
 import me.roundaround.inventorymanagement.inventory.sorting.CachingComparatorImpl;
 import me.roundaround.inventorymanagement.inventory.sorting.LexicographicalListComparator;
-import me.roundaround.inventorymanagement.inventory.sorting.PredicatedComparator;
+import me.roundaround.inventorymanagement.inventory.sorting.SerialComparator;
 import net.minecraft.block.entity.Sherds;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Language;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class DecoratedPotComparator extends CachingComparatorImpl<ItemStack, List<String>> {
+public class DecoratedPotComparator extends CachingComparatorImpl<ItemStack,
+    DecoratedPotComparator.DecoratedPotSummary> {
   public DecoratedPotComparator() {
-    //@formatter:off
-    super(PredicatedComparator.ignoreNulls(
-        LexicographicalListComparator.comparing(
-            Language.getInstance()::get,
-            Comparator.nullsLast(String::compareToIgnoreCase)
-        )
-    ));
-    //@formatter:on
+    super(Comparator.naturalOrder());
   }
 
   @Override
-  protected List<String> mapValue(ItemStack stack) {
-    Sherds component = stack.get(DataComponentTypes.POT_DECORATIONS);
-    if (component == null) {
-      return null;
+  protected DecoratedPotSummary mapValue(ItemStack stack) {
+    return DecoratedPotSummary.of(stack);
+  }
+
+  protected record DecoratedPotSummary(int count, List<String> translated) implements Comparable<DecoratedPotSummary> {
+    private static Comparator<DecoratedPotSummary> comparator;
+
+    public static DecoratedPotSummary of(ItemStack stack) {
+      Sherds component = stack.get(DataComponentTypes.POT_DECORATIONS);
+      if (component == null) {
+        return new DecoratedPotSummary(0, List.of());
+      }
+
+      List<Item> items = component.stream();
+
+      int count = 0;
+      ArrayList<String> translated = new ArrayList<>();
+
+      Language language = Language.getInstance();
+      for (Item item : items) {
+        if (item != Items.BRICK) {
+          count++;
+          translated.add(language.get(item.getTranslationKey()));
+        } else {
+          translated.add(null);
+        }
+      }
+
+      return new DecoratedPotSummary(count, translated);
     }
-    return component.stream().stream().map((item) -> item == Items.BRICK ? null : item.getTranslationKey()).toList();
+
+    @Override
+    public int compareTo(@NotNull DecoratedPotSummary other) {
+      return getComparator().compare(this, other);
+    }
+
+    private static Comparator<DecoratedPotSummary> getComparator() {
+      if (comparator == null) {
+        //@formatter:off
+        comparator = SerialComparator.comparing(
+            Comparator.comparingInt(DecoratedPotSummary::count),
+            Comparator.comparing(
+                DecoratedPotSummary::translated,
+                LexicographicalListComparator.comparing(Comparator.nullsLast(String::compareToIgnoreCase))
+            ));
+        //@formatter:on
+      }
+      return comparator;
+    }
   }
 }
---
+++ src/test/java/me/roundaround/inventorymanagement/inventory/sorting/itemstack/DecoratedPotComparatorTest.java
@@ -0,0 +1,223 @@
+package me.roundaround.inventorymanagement.inventory.sorting.itemstack;
+
+import com.google.common.collect.Lists;
+import me.roundaround.inventorymanagement.testing.BaseMinecraftTest;
+import net.minecraft.block.entity.Sherds;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static me.roundaround.inventorymanagement.testing.AssertIterableMatches.selectNames;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+public class DecoratedPotComparatorTest extends BaseMinecraftTest {
+  @Test
+  void ignoresActualItem() {
+    //@formatter:off
+    ArrayList<ItemStack> samples = Lists.newArrayList(
+        createEmpty(Items.NETHERITE_CHESTPLATE),
+        createEmpty(Items.RED_BANNER),
+        createEmpty(Items.DIAMOND_CHESTPLATE),
+        createEmpty(Items.FIRE_CHARGE),
+        createEmpty(Items.DECORATED_POT),
+        createEmpty(Items.BLUE_BANNER),
+        createEmpty(Items.BAMBOO)
+    );
+    //@formatter:on
+
+    DecoratedPotComparator comparator = new DecoratedPotComparator();
+    for (ItemStack a : samples) {
+      for (ItemStack b : samples) {
+        assertEquals(0, comparator.compare(a, b));
+      }
+    }
+  }
+
+  @Test
+  void treatsEmptyDecorationsComponentAsEqual() {
+    ItemStack a = new Builder().name("a").build();
+    ItemStack b = new Builder().name("b").build();
+
+    DecoratedPotComparator comparator = new DecoratedPotComparator();
+    assertEquals(0, comparator.compare(a, b));
+  }
+
+  @Test
+  void sortsByCountAsc() {
+    //@formatter:off
+    ArrayList<ItemStack> actual = Lists.newArrayList(
+        new Builder().name("1")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD)
+            .front(Items.SCRAPE_POTTERY_SHERD).build(),
+        new Builder().name("2")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD).build(),
+        new Builder().name("3")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD).build()
+    );
+    //@formatter:on
+
+    actual.sort(new DecoratedPotComparator());
+
+    assertIterableEquals(List.of("2", "3", "1"), selectNames(actual));
+  }
+
+  @Test
+  void sortsByCountThenAlphabetically() {
+    //@formatter:off
+    ArrayList<ItemStack> actual = Lists.newArrayList(
+        new Builder().name("1")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD)
+            .front(Items.SCRAPE_POTTERY_SHERD).build(),
+        new Builder().name("2")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD).build(),
+        new Builder().name("3")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FRIEND_POTTERY_SHERD).build(),
+        new Builder().name("4")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD).build()
+    );
+    //@formatter:on
+
+    actual.sort(new DecoratedPotComparator());
+
+    assertIterableEquals(List.of("2", "4", "3", "1"), selectNames(actual));
+  }
+
+  @Test
+  void sortsByBackLeftRightFrontAlphabetically() {
+    //@formatter:off
+    ArrayList<ItemStack> actual = Lists.newArrayList(
+        new Builder().name("1")
+            .back(Items.ARMS_UP_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD)
+            .front(Items.SCRAPE_POTTERY_SHERD).build(),
+        new Builder().name("2")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FRIEND_POTTERY_SHERD)
+            .front(Items.SCRAPE_POTTERY_SHERD).build(),
+        new Builder().name("3")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD)
+            .front(Items.SCRAPE_POTTERY_SHERD).build(),
+        new Builder().name("4")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BREWER_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD)
+            .front(Items.SCRAPE_POTTERY_SHERD).build(),
+        new Builder().name("5")
+            .back(Items.ARCHER_POTTERY_SHERD)
+            .left(Items.BLADE_POTTERY_SHERD)
+            .right(Items.FLOW_POTTERY_SHERD)
+            .front(Items.SKULL_POTTERY_SHERD).build()
+    );
+    //@formatter:on
+
+    actual.sort(new DecoratedPotComparator());
+
+    assertIterableEquals(List.of("3", "5", "2", "4", "1"), selectNames(actual));
+  }
+
+  @Test
+  void sortsEmptyLast() {
+    //@formatter:off
+    ArrayList<ItemStack> actual = Lists.newArrayList(
+    new Builder().name("1") // left empty
+        .back(Items.ARCHER_POTTERY_SHERD)
+        .right(Items.FLOW_POTTERY_SHERD)
+        .front(Items.SCRAPE_POTTERY_SHERD).build(),
+    new Builder().name("2") // front empty
+        .back(Items.ARCHER_POTTERY_SHERD)
+        .left(Items.BREWER_POTTERY_SHERD)
+        .right(Items.FLOW_POTTERY_SHERD).build(),
+    new Builder().name("3") // right empty
+        .back(Items.ARCHER_POTTERY_SHERD)
+        .left(Items.BREWER_POTTERY_SHERD)
+        .front(Items.SCRAPE_POTTERY_SHERD).build()
+    );
+    //@formatter:on
+
+    actual.sort(new DecoratedPotComparator());
+
+    assertIterableEquals(List.of("2", "3", "1"), selectNames(actual));
+  }
+
+  private static ItemStack createEmpty(Item item) {
+    ItemStack stack = new ItemStack(item);
+    stack.remove(DataComponentTypes.POT_DECORATIONS);
+    return stack;
+  }
+
+  private static class Builder {
+    private Item item = Items.DECORATED_POT;
+    private String name = null;
+    private Optional<Item> back = Optional.empty();
+    private Optional<Item> left = Optional.empty();
+    private Optional<Item> right = Optional.empty();
+    private Optional<Item> front = Optional.empty();
+
+    public Builder item(Item item) {
+      this.item = item;
+      return this;
+    }
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder back(Item back) {
+      this.back = Optional.of(back);
+      return this;
+    }
+
+    public Builder left(Item left) {
+      this.left = Optional.of(left);
+      return this;
+    }
+
+    public Builder right(Item right) {
+      this.right = Optional.of(right);
+      return this;
+    }
+
+    public Builder front(Item front) {
+      this.front = Optional.of(front);
+      return this;
+    }
+
+    public ItemStack build() {
+      ItemStack stack = new ItemStack(this.item);
+
+      if (this.name != null && !this.name.isBlank()) {
+        stack.set(DataComponentTypes.CUSTOM_NAME, Text.of(this.name));
+      }
+
+      Sherds sherds = new Sherds(this.back, this.left, this.right, this.front);
+      stack.set(DataComponentTypes.POT_DECORATIONS, sherds);
+
+      return stack;
+    }
+  }
+}
